99e8d12986003c6b5248251cb973b4ffe8481f91
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-06-25 12:30:44 -0300

    almost finished challenge 01

--- desafio-01/index.js
+++ desafio-01/index.js
@@ -1,76 +1,77 @@
-const express = require('express')
-const server = express()
+const express = require("express");
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
 const projects = [
-    {
-        id: '1123',
-        title: 'Project 1',
-        tasks: [
-            'task1',
-            'task2'
-        ]
-    },
-    {
-        id: '222',
-        title: 'Project 2',
-        tasks: [
-            'task3',
-            'task4'
-        ]
+  {
+    id: "1123",
+    title: "Project 1",
+    tasks: ["task1", "task2"]
+  },
+  {
+    id: "222",
+    title: "Project 2",
+    tasks: ["task3", "task4"]
+  }
+];
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+  try {
+    projects.push({
+      id,
+      title,
+      tasks: []
+    });
+
+    res.json({ message: "Created with success!" });
+  } catch ({ message: error }) {
+    res.status(400).json({ error });
+  }
+});
+
+server.get("/projects", (req, res) => {
+  try {
+    res.json(projects);
+  } catch ({ message: error }) {
+    res.status(500).send({ error });
+  }
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  try {
+    const projectIndex = projects.findIndex(project => project.id == id);
+    if (projectIndex === -1) {
+      return res.status(404).json({ error: "Project not found" });
     }
-]
+    projects[projectIndex].title = req.body.title;
+    res.json(projects[projectIndex]);
+  } catch ({ message: error }) {
+    res.status(500).send({ error });
+  }
+});
 
-server.post('/projects', (req, res) => {
-    const { id, title } = req.body
-    try {
-        projects.push({
-            id,
-            title,
-            tasks: []
-        })
-
-        res.status(401).json({ message: 'Created with success!' })
-
-    } catch ({ message: error }) {
-        res.status(400).json({ error })
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  try {
+    const projectIndex = projects.findIndex(project => project.id == id);
+    if (projectIndex === -1) {
+      return res.status(404).json({ error: "Project not found" });
     }
-})
+    projects.splice(projectIndex, 1);
+    res.json({
+      message: "Project deleted with success!"
+    });
+  } catch ({ message: error }) {
+    res.status(400).json({ error });
+  }
+});
 
-server.get('/projects', (req, res) => {
-    try {
-        res.json(projects)
-    } catch ({ message: error }) {
-        res.status(500).send({ error })
-    }
-})
+server.post("/projects/:id/tasks", (req, res) => {});
 
-server.put('/projects/:id', (req, res) => {
-    const { id } = req.params
-    try {
-        const projectIndex = projects.findIndex((project) => project.id == id)
-        if (projectIndex === -1) {
-            return res.status(404).json({ error: 'Project not found' })
-        }
-        projects[projectIndex] = {
-            title: req.body.title
-        }
-        res.json(projects[projectIndex])
-    } catch ({ message: error }) {
-        res.status(500).send({ error })
-    }
-})
-
-server.delete('/projects/:id', (req, res) => {
-
-})
-
-server.post('/projects/:id/tasks', (req, res) => {
-
-})
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
