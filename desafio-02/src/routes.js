e2e65992dd71b6d4a3819bb53dcc17535a50815c
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-06-26 23:47:01 -0300

    desafio 02 finalizado

--- desafio-02/src/routes.js
+++ desafio-02/src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
-import UserController from '../src/app/controllers/UserController';
-// import SessionController from '';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from '../src/app/middlewares/auth';
 
 const routes = new Router();
 
-// routes.post('/sessions', SessionController.store);
+routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
