433ec5ff897b230230ee0bf24a7a2b6ccdd84076
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-06-27 11:43:36 -0300

    modulo03 aula 3

--- modulo03/src/database/index.js
+++ modulo03/src/database/index.js
@@ -17,7 +17,10 @@ class Database {
 
     models
       .map(model => model.init(this.connection))
-      .map(model => model.associate && model.associate(this.connection.models));
+      .map(
+        model =>
+          model && model.associate && model.associate(this.connection.models)
+      );
   }
 }
 
