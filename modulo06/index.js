b599495c4102f7ae5268469d978ecd9db7686661
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-07-07 23:15:58 -0300

    modulo06 react-native finalizado e gitignore atualizado

--- modulo06/index.js
+++ modulo06/index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+/**
+ * 1) rodei yarn add react-navigation e yarn add react-native-gesture-handler
+ * 2) rodei react-native link react-native-gesture-handler
+ * 3) rodei yarn add jetifier (?)
+ * 4) rodei npx jetify (?)
+ */
