bd07df71b03c522d63ee338bf2f38cad8a03b26c
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-07-10 08:22:09 -0300

    modulo07 estilização da home page

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -20,6 +20,81 @@ export default function Home() {
           </div>
         </button>
       </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis caminhada</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis caminhada</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis caminhada</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis caminhada</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis caminhada</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
     </ProductList>
   );
 }
