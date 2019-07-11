774390c8b0fc7fa32fabd87c152a53d68cf50e2b
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-07-10 21:34:08 -0300

    modulo07 redux calculando totais e refatorando actions para reutilização

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
@@ -28,12 +31,9 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
+    const { addToCart } = this.props;
 
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    addToCart(product);
   };
 
   render() {
@@ -63,4 +63,10 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home);
