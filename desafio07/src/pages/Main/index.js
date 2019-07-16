2cdcab8c764712199f91b2e496a4a6bc02cc91ea
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-07-16 20:20:25 -0300

    modulo08 react hooks básicos

---
+++ desafio07/src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Container, Text1 } from './styles';
+
+export default class Main extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Container>
+        <Text1>oi</Text1>
+      </Container>
+    );
+  }
+}
