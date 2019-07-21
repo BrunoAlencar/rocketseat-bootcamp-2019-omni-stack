58f59cda8f83e06435481696344464c2b5376e0c
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2019-07-21 12:28:20 -0300

    modulo09 logout da aplicação, dados do header, foto perfil, atualizando perfil

--- modulo09/src/pages/Profile/index.js
+++ modulo09/src/pages/Profile/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Container } from './styles';
 
+import AvatarInput from './AvatarInput';
+
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    console.tron.log('HANDLE SUBMIT', data);
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
         <Input type="email" name="email" placeholder="Seu endereço de email" />
 
@@ -31,7 +46,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
--- modulo09/src/store/modules/user/sagas.js
+++ modulo09/src/store/modules/user/sagas.js
@@ -1,3 +1,29 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    console.tron.log('payload', payload);
+    const { name, email, avatar_id, ...rest } = payload;
+    const profile = Object.assign(
+      { name, email, avatar_id },
+      rest.oldPassword ? rest : {}
+    );
+    console.tron.log('profile', profile);
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error('Erro ao atualizar perfil, confira seus dados.');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
